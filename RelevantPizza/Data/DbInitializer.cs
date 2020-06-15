2401f08d86c37edda041158f3d0d4f6630a5e905
akzin05 <aungkhantzin50@gmail.com>
2020-06-16 05:36:25 +0800

    Entity Framework Setup

---
+++ RelevantPizza/Data/DbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RelevantPizza.Models;
+
+namespace RelevantPizza.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize (PizzaContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Employees.Any())
+            {
+                return;
+            }
+
+            Employee[] employees = new Employee[]
+            {
+                new Employee{FirstName="Jessee", LastName="Prescot", PhoneNumber="12345", Role=Roles.Manager,Salary=4000},
+                new Employee{FirstName="Jim", LastName="Smith", PhoneNumber="12345", Role=Roles.AssitantManager,Salary=3500},
+                new Employee{FirstName="Bob", LastName="Anderson", PhoneNumber="12345", Role=Roles.Cashier,Salary=30000}
+            };
+
+            context.Employees.AddRange(employees);
+            //foreach(Employee employee in employees)
+            //{
+            //    context.Employees.Add(employee);
+            //}
+            //use Addrange instead of Add no need foreach
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ RelevantPizza/Data/PizzaContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RelevantPizza.Models;
+
+namespace RelevantPizza.Data
+{
+    public class PizzaContext : DbContext
+    {
+        public PizzaContext(DbContextOptions<PizzaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        //protected override void OnModelCreating (ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Employee>().ToTable("Employee");
+        //}
+        // if you don't want tabel name with Employees use this method
+    }
+}
